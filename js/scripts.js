64d98655102b28075bd6a57eae1c0b826fd61309
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-24 23:24:08 -0600

    Add form for name, date, start and end time, and submit button and image; Style heading

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#user").submit(function(event) {
+    event.preventDefault();
+    var userName = $("#user-name").val();
+    var selectedDate = $("#date").val();
+    var startTime = $("#start").val();;
+    var endTime = $("#end").val();
+    var listReservation = function();
+  });
+});
